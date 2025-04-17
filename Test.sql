e7fbc69472640d947b31a8f6cb9cab116036d82d
Bekhzod <ergashovb.ds@gmail.com>
2025-04-17 17:47:09 +0500

    Add files via upload

---
+++ Test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees ( id INT PRIMARY KEY, name VARCHAR(100), salary DECIMAL(10, 2) );
+INSERT INTO employees (id, name, salary) VALUES (1, 'Alice', 50000), (2, 'Bob', 60000), (3, 'Charlie', 50000);
+
+
+SELECT * FROM employees;
+
+-- Test 2
+
